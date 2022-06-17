c5400f820ad2bc9976773ea90772d94961da26ed
FrancoPiedrabuena <franconpiedrabuena@gmail.com>
2022-06-17 19:22:34 +0000

    mi generador de dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generadorDominio() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var paginaweb = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        paginaweb += pronoun[i] + adj[a] + noun[b] + ".com" + " ";
+      }
+    }
+  }
+  return paginaweb;
+}
+console.log(generadorDominio());
