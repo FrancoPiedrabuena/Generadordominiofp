38d5ee1e117fb1253bb22ac724a1a86e8624bf89
FrancoPiedrabuena <franconpiedrabuena@gmail.com>
2022-06-20 18:52:04 +0000

    funcionando sin errores

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 function generadorDominio() {
   var pronoun = ["the", "our"];
   var adj = ["great", "big"];
